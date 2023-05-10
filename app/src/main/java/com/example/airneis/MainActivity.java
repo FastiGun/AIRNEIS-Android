57fd8b225bea2ff2f67c66594d6312a2537c38a5
Alexis <alexis.fevrie@gmail.com>
2023-05-10 15:36:04 +0200

    HomePage

--- app/src/main/java/com/example/airneis/MainActivity.java
+++ app/src/main/java/com/example/airneis/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.airneis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Fragment homePageFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        homePageFragment = new HomePageFragment();
+        loadFragment(homePageFragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragmentContainer, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
