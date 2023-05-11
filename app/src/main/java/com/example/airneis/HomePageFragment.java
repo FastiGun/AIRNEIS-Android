3bf11a97e518e143c93ed1323c12cde8c2440bde
Alexis <alexis.fevrie@gmail.com>
2023-05-11 11:21:11 +0200

    HomePage Caroussel

--- app/src/main/java/com/example/airneis/HomePageFragment.java
+++ app/src/main/java/com/example/airneis/HomePageFragment.java
@@ -1,6 +1,7 @@
 package com.example.airneis;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,12 +9,56 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class HomePageFragment extends Fragment {
+
+    ViewPager carousselViewPager;
+    Handler handler;
+    Runnable runnable;
+    int currentImage = 0;
+
+    String[] imageUrls = new String[]{
+        "https://www.for-interieur.fr/wp-content/uploads/2017/12/enfilade-scandinave-2.jpg",
+    "https://www.for-interieur.fr/wp-content/uploads/2017/12/enfilade-scandinave-2.jpg",
+    "https://www.for-interieur.fr/wp-content/uploads/2017/12/enfilade-scandinave-2.jpg"
+    };
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.home_page_fragment, null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        carousselViewPager = view.findViewById(R.id.carousselViewPager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this.getContext(), imageUrls);
+        carousselViewPager.setAdapter(adapter);
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                changeImage();
+            }
+        };
+        handler.postDelayed(runnable, 3000);
+    }
+
+    private void changeImage() {
+        currentImage++;
+        if (currentImage == imageUrls.length) {
+            currentImage = 0;
+        }
+        carousselViewPager.setCurrentItem(currentImage, true);
+        handler.postDelayed(runnable, 3000);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
     }
 }
--- app/src/main/java/com/example/airneis/modeles/Favoris.java
+++ app/src/main/java/com/example/airneis/modeles/Favoris.java
@@ -8,14 +8,68 @@ public class Favoris {
     Produit produit1;
     Produit produit2;
     Produit produit3;
+    String caroussel1;
+    String caroussel2;
+    String caroussel3;
 
-    public Favoris(Categorie categorie1, Categorie categorie2, Categorie categorie3, Produit produit1, Produit produit2, Produit produit3) {
+    public Favoris(Categorie categorie1, Categorie categorie2, Categorie categorie3, Produit produit1, Produit produit2, Produit produit3, String caroussel1, String caroussel2, String caroussel3) {
         this.categorie1 = categorie1;
         this.categorie2 = categorie2;
         this.categorie3 = categorie3;
         this.produit1 = produit1;
         this.produit2 = produit2;
         this.produit3 = produit3;
+        this.caroussel1 = caroussel1;
+        this.caroussel2 = caroussel2;
+        this.caroussel3 = caroussel3;
+    }
+
+    public Categorie getCategorie1() {
+        return categorie1;
+    }
+
+    public Categorie getCategorie2() {
+        return categorie2;
+    }
+
+    public Categorie getCategorie3() {
+        return categorie3;
+    }
+
+    public Produit getProduit1() {
+        return produit1;
+    }
+
+    public Produit getProduit2() {
+        return produit2;
+    }
+
+    public Produit getProduit3() {
+        return produit3;
+    }
+
+    public String getCaroussel1() {
+        return caroussel1;
+    }
+
+    public String getCaroussel2() {
+        return caroussel2;
+    }
+
+    public String getCaroussel3() {
+        return caroussel3;
+    }
+
+    public void setCaroussel1(String caroussel1) {
+        this.caroussel1 = caroussel1;
+    }
+
+    public void setCaroussel2(String caroussel2) {
+        this.caroussel2 = caroussel2;
+    }
+
+    public void setCaroussel3(String caroussel3) {
+        this.caroussel3 = caroussel3;
     }
 
     public void setCategorie1(Categorie categorie1) {
