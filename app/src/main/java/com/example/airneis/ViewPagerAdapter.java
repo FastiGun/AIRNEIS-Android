b31ae25a3c989391b1c398cc1adc4f56e498a668
Alexis <alexis.fevrie@gmail.com>
2023-05-11 09:25:52 +0200

    Caroussel Adapter

---
+++ app/src/main/java/com/example/airneis/ViewPagerAdapter.java
@@ -0,0 +1,52 @@
+package com.example.airneis;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.squareup.picasso.Picasso;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    private Context context;
+    private String[] imagesUrls;
+
+    public ViewPagerAdapter(Context context, String[] imagesUrls) {
+        this.context = context;
+        this.imagesUrls = imagesUrls;
+    }
+
+    @Override
+    public int getCount() {
+        return imagesUrls.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        ImageView imageView = new ImageView(context);
+        Picasso.get()
+                .load(imagesUrls[position])
+                .fit()
+                .centerCrop()
+                .into(imageView);
+        container.addView(imageView);
+
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
